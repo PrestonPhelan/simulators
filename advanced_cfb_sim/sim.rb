727d65c0651f526c91f3e5c0dfd1ba9d00417de0
Preston Phelan <preston.phelan@gmail.com>
2016-12-04 10:34:31 -0800

    Added explicit simulation call methods to sim

--- advanced_cfb_sim/sim.rb
+++ advanced_cfb_sim/sim.rb
@@ -1,5 +1,6 @@
 require_relative 'team'
 require_relative 'scheduler'
+require_relative 'season'
 require 'byebug'
 require 'rubystats'
 
@@ -27,41 +28,56 @@ def new_division_hash(teams)
   divisions
 end
 
-def champ_sim(team1, team2)
-  champ = sim_game(team1, team2, true)
-  puts "#{champ} wins their conference championship!"
-  champ.conf_champ += 1
+def get_sims(playoff_size, num)
+  sims = Array.new
+  num.times do
+    new_season = Season.new(playoff_size)
+    new_season.run
+    sims << new_season
+  end
+
+  sims
 end
 
-def get_playoff_teams(teams, spots = 4)
-  ranked = ranker(teams)
-  ranked_list(ranked, 25)
-  ranked.take(spots)
+def champ_counts(sims, rank)
+  sims.map { |season| season.champion.true_rank }.count(rank)
 end
-
 
 
 if __FILE__ == $0
-  best_team_wins = 0
-  best_made_playoffs = 0
-  undefeateds = 0
-  conferences_represented = 0
-  all_different = 0
-  1000000.times do
-    teams = Team.create_all
-    results = run_season(teams)
-    best_team_wins += 1 if results[0].true_rank == 1
-    best_made_playoffs += 1 if results[1]
-    undefeateds += results[2]
-    conferences_represented += results[3]
-    all_different += 1 if results[4]
+  two_team_sims = []
+  1000.times do
+    new_season = Season.new(2)
+    new_season.run
+    two_team_sims << new_season
   end
 
-  puts "Best team won #{best_team_wins} times out of a million."
-  puts "Best team made playoffs #{best_made_playoffs} times."
-  puts "There were an average of #{undefeateds / 1000000.0} undefeated regular season teams."
-  puts "There were an average of #{conferences_represented / 1000000.0} conferences in the playoffs."
-  puts "Four different conferences were represented #{all_different} times."
+  four = []
+  1000.times do
+    new_season = Season.new(2)
+    new_season.run
+    two_team_sims << new_season
+  end
+  # best_team_wins = 0
+  # best_made_playoffs = 0
+  # undefeateds = 0
+  # conferences_represented = 0
+  # all_different = 0
+  # 1000000.times do
+  #   teams = Team.create_all
+  #   results = run_season(teams)
+  #   best_team_wins += 1 if results[0].true_rank == 1
+  #   best_made_playoffs += 1 if results[1]
+  #   undefeateds += results[2]
+  #   conferences_represented += results[3]
+  #   all_different += 1 if results[4]
+  # end
+  #
+  # puts "Best team won #{best_team_wins} times out of a million."
+  # puts "Best team made playoffs #{best_made_playoffs} times."
+  # puts "There were an average of #{undefeateds / 1000000.0} undefeated regular season teams."
+  # puts "There were an average of #{conferences_represented / 1000000.0} conferences in the playoffs."
+  # puts "Four different conferences were represented #{all_different} times."
   # 5.times do
   #   sample = teams.sample
   #   puts "#{sample.name}: #{sample.wins}-#{sample.losses} (#{sample.conf_wins}-#{sample.conf_losses})"
