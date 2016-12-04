a1edfc7bf70515c59bc6fa48a9fff0d355d68b6b
Preston Phelan <preston.phelan@gmail.com>
2016-12-03 23:12:15 -0800

    Finished full schedule generation functionality

--- advanced_cfb_sim/scheduler.rb
+++ advanced_cfb_sim/scheduler.rb
@@ -1,57 +1,91 @@
 require_relative 'team'
 require_relative 'sim'
+require 'byebug'
 
+G5_RATINGS = [75, 74, 73, 55, 33, 55, 24, 53, 42, 44, 56].freeze
+
+
+def generate_full_schedule(teams)
+  games = Array.new
+
+  conference_hash = new_conference_hash(teams)
+  division_hash = new_division_hash(teams)
+
+  games += g5_games(teams)
+  games += generate_non_conference_games(conference_hash)
+  games += seven_team_division_games(division_hash, :ACC)
+  games += seven_team_division_games(division_hash, :Big_Ten)
+  games += seven_team_division_games(division_hash, :SEC)
+  games += large_interdivision_games(division_hash, :ACC)
+  games += large_interdivision_games(division_hash, :Big_Ten)
+  games += large_interdivision_games(division_hash, :SEC)
+  games += six_team_division_games(division_hash, :PAC_12)
+  games += pac_12_interdivision_games(division_hash)
+  games += big_12_games(conference_hash)
+
+  games
+end
 ##Non-Conference Schedule
 ##Each team gets a random game against another non-conference P5 school
-def generate_non_conference_games
-  team_hash = new_conference_hash(Team.create_all)
-  team_hash.each do |k, v|
-    team_hash[k] = v.shuffle
+def generate_non_conference_games(conference_hash)
+  clone_hash = conference_hash.clone
+  clone_hash.each do |k, v|
+    clone_hash[k] = v.shuffle
   end
 
   games = Array.new
 
   4.times do
-    games << take_teams(team_hash, :ACC, :SEC)
-    games << take_teams(team_hash, :Big_Ten, :ACC)
-    games << take_teams(team_hash, :Big_Ten, :SEC)
-    games << take_teams(team_hash, :Big_Ten, :PAC_12)
+    games << take_teams(clone_hash, :ACC, :SEC)
+    games << take_teams(clone_hash, :Big_Ten, :ACC)
+    games << take_teams(clone_hash, :Big_Ten, :SEC)
+    games << take_teams(clone_hash, :Big_Ten, :PAC_12)
   end
 
   3.times do
-    games << take_teams(team_hash, :ACC, :PAC_12)
-    games << take_teams(team_hash, :ACC, :Big_12)
-    games << take_teams(team_hash, :SEC, :PAC_12)
-    games << take_teams(team_hash, :SEC, :Big_12)
+    games << take_teams(clone_hash, :ACC, :PAC_12)
+    games << take_teams(clone_hash, :ACC, :Big_12)
+    games << take_teams(clone_hash, :SEC, :PAC_12)
+    games << take_teams(clone_hash, :SEC, :Big_12)
   end
 
-  2. times do
-    games << take_teams(team_hash, :Big_Ten, :Big_12)
-    games << take_teams(team_hash, :Big_12, :PAC_12)
+  2.times do
+    games << take_teams(clone_hash, :Big_Ten, :Big_12)
+    games << take_teams(clone_hash, :Big_12, :PAC_12)
   end
 
-  team_hash.each do |k, v|
+  clone_hash.each do |k, v|
     raise "Didn't properly empty #{k}, still has #{v}" unless v.empty?
   end
 
   games
 end
 
-def take_teams(team_hash, conf1, conf2)
-  matchup = [team_hash[conf1].pop, team_hash[conf2].pop]
+def take_teams(conference_hash, conf1, conf2)
+  matchup = [conference_hash[conf1].pop, conference_hash[conf2].pop]
   matchup.shuffle
 end
 
-  ##Each team gets a two home games agains G5 schools
-  #G5_TEAMS.shuffle.take(2)
+##Each team gets a two home games agains G5 schools
+#G5_TEAMS.shuffle.take(2)
+def g5_games(teams)
+  games = Array.new
+  teams.each do |team|
+    2.times do
+      games << [team, G5_RATINGS.sample]
+    end
+  end
+
+  games
+end
 
 ##Conference Schedule, CONSTANT sim-to-sim
-  ##14-team conferences: 6 division games, 3 other division
+##14-team conferences: 6 division games, 3 other division
 def seven_team_division_games(division_hash, conference)
   games = Array.new
   divisions = TEAMS[conference].keys
   divisions.each do |division|
-    teams = division_hash["#{conference.to_s} #{division.to_s}"].shuffle
+    teams = division_hash["#{conference} #{division}"].shuffle
 
     teams.each_with_index do |team, idx|
       games << [team, teams[idx - 1]]
@@ -67,7 +101,7 @@ def six_team_division_games(division_hash, conference)
   games = Array.new
   divisions = TEAMS[conference].keys
   divisions.each do |division|
-    teams = division_hash["#{conference.to_s} #{division.to_s}"].shuffle
+    teams = division_hash["#{conference} #{division}"].shuffle
 
     teams.each_with_index do |team, idx|
       games << [team, teams[idx - 1]]
@@ -83,8 +117,8 @@ def large_interdivision_games(division_hash, conference)
   games = Array.new
   divisions = TEAMS[conference].keys.shuffle
 
-  first_division = division_hash["#{conference.to_s} #{divisions[0].to_s}"].shuffle
-  second_division = division_hash["#{conference.to_s} #{divisions[1].to_s}"].shuffle
+  first_division = division_hash["#{conference} #{divisions[0]}"].shuffle
+  second_division = division_hash["#{conference} #{divisions[1]}"].shuffle
 
   first_division.each_with_index do |team, idx|
     games << [team, second_division[idx - 1]]
@@ -105,24 +139,24 @@ def pac_12_interdivision_games(division_hash)
   south = division_hash["PAC_12 South"].shuffle
 
   north.each_with_index do |team, idx|
-    2.times { |i| games << [team, south[idx - i]] }
+    2.times { |i| games << [team, south[idx - i - 1]] }
   end
 
   north.each_with_index do |team, idx|
-    2.times { |i| games << [team, south[idx - i]] }
+    2.times { |i| games << [team, south[idx - i - 1]] }
   end
 
   games
 end
-  ##Pac-12: 5 division game, 4 other division
-  ##Big 12: Every other team round robin
+
 def big_12_games(conference_hash)
   games = Array.new
+
   teams = conference_hash[:Big_12].shuffle
 
   teams.each_with_index do |team, idx|
     4.times do |i|
-      games << [team, teams[idx - i]]
+      games << [team, teams[idx - i - 1]]
     end
     games << [team, teams[idx - 5]] if idx < 5
   end
@@ -148,3 +182,29 @@ end
 # division_hash["Big_Ten West"].each do |team|
 #   puts "#{games.flatten.count(team)} games for #{team}"
 # end
+
+# teams = Team.create_all
+# games = big_12_games(new_conference_hash(teams))
+
+# puts games.class
+#
+# games.each do |matchup|
+#   print matchup.map(&:to_s)
+#   puts
+# end
+#
+# puts "#{games.length} games generated."
+#
+teams = Team.create_all
+games = generate_full_schedule(teams)
+
+puts "#{games.length} games generated, #{32 * 10 + 128} expected"
+5.times do
+  print games.sample.map(&:to_s)
+  puts
+end
+
+teams.each do |team|
+  team_games = games.flatten.count(team)
+  puts "Error: #{team_games} games generated for #{team}" unless team_games == 12
+end
